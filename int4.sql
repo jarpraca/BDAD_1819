30fd16bf4597eccdc3e3cec56908f97631e60f32
João Praça <jarpraca@gmail.com>
2019-05-25 21:48:05 +0100

    Fixed int4

--- int4.sql
+++ int4.sql
@@ -4,8 +4,7 @@ PRAGMA foreign_keys = ON;
 .headers	on
 .nullvalue	NULL
 
---SELECT DISTINCT Habitacao.idHabitacao, Habitacao.precoNoite*('2019-07-30'- '2019-07-02')+Habitacao.taxaLimpeza AS precoTotal
-SELECT DISTINCT Habitacao.idHabitacao, Habitacao.precoNoite*7+Habitacao.taxaLimpeza AS precoTotal
-FROM  Habitacao, Reserva, Disponivel
-WHERE ( Habitacao.idCidade = 7 AND Habitacao.maxHospedes>2  AND Habitacao.numQuartos= 3 AND (Disponivel.idHabitacao= Habitacao.idHabitacao AND Disponivel.data>='2019-07-23' AND Disponivel.data<='2019-07-30'))
-order by precoTotal;
+SELECT DISTINCT idHabitacao, precoNoite*(JulianDay('2019-07-30') - JulianDay('2019-07-25'))+taxaLimpeza AS precoT
+FROM (Habitacao NATURAL JOIN Disponivel) AS P
+WHERE idCidade = 7 AND maxHospedes >= 2 AND data >= '2019-07-25' AND data <= '2019-07-30'
+ORDER BY precoT;
