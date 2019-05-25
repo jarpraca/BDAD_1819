0d7f0f75293e360455b628a4033329ee7119dd5b
SilviaRocha <silviajmr99@gmail.com>
2019-05-25 18:48:09 +0100

    int 4 and 7 kinda done

--- int4.sql
+++ int4.sql
@@ -4,7 +4,8 @@ PRAGMA foreign_keys = ON;
 .headers	on
 .nullvalue	NULL
 
-SELECT Habitacao.idHabitacao, (Habitacao.precoNoite*(DATEDIFF(day, Habitacao.dataCheckIn, Habitacao.dataCheckOut)+Habitacao.taxalimpeza)
-FROM  Habitacao
-WHERE ( Habitacao.idCidade = 1 AND Habitacao.maxHospedes>4  AND Habitacao.numQuartos= 2)
-order by Habitacao.idHabitacao;
+--SELECT DISTINCT Habitacao.idHabitacao, Habitacao.precoNoite*('2019-07-30'- '2019-07-02')+Habitacao.taxaLimpeza AS precoTotal
+SELECT DISTINCT Habitacao.idHabitacao, Habitacao.precoNoite*7+Habitacao.taxaLimpeza AS precoTotal
+FROM  Habitacao, Reserva, Disponivel
+WHERE ( Habitacao.idCidade = 7 AND Habitacao.maxHospedes>2  AND Habitacao.numQuartos= 3 AND (Disponivel.idHabitacao= Habitacao.idHabitacao AND Disponivel.data>='2019-07-23' AND Disponivel.data<='2019-07-30'))
+order by precoTotal;
