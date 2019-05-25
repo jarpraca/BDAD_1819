cdd4c880d3db3ad46aecfffc13b8d1b8adc58db6
SilviaRocha <silviajmr99@gmail.com>
2019-05-25 15:27:55 +0100

    int 1,2 and 5 made

---
+++ int2.sql
@@ -0,0 +1,10 @@
+PRAGMA foreign_keys = ON;
+
+.mode	columns
+.headers	on
+.nullvalue	NULL
+
+SELECT Habitacao.idHabitacao, Cidade.nome
+FROM Possui, Habitacao, Cidade
+WHERE (Cidade.idCidade = Habitacao.idCidade AND Possui.idHabitacao = Habitacao.idHabitacao AND Possui.idAnfitriao = 1)
+order by Habitacao.idHabitacao;
---
+++ int4.sql
@@ -0,0 +1,10 @@
+PRAGMA foreign_keys = ON;
+
+.mode	columns
+.headers	on
+.nullvalue	NULL
+
+SELECT Habitacao.idHabitacao, (Habitacao.precoNoite*(DATEDIFF(day, Habitacao.dataCheckIn, Habitacao.dataCheckOut)+Habitacao.taxalimpeza)
+FROM  Habitacao
+WHERE ( Habitacao.idCidade = 1 AND Habitacao.maxHospedes>4  AND Habitacao.numQuartos= 2)
+order by Habitacao.idHabitacao;
